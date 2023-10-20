37ea42381e00e58e1f795613a2261be1c18461d6
Carlos <dag45684@educastur.es>
2023-10-20 20:17:34 +0200

    its working as intended

--- ytpld.py
+++ ytpld.py
@@ -1,13 +1,11 @@
 import os
 import time
+import re
 
 try:
     from pytube import Playlist, YouTube
 except ModuleNotFoundError:
     print('Please install pytube for the program to work.')
-
-
-###
 
 def get_urls(pl):
     urls = []
@@ -17,29 +15,29 @@ def get_urls(pl):
 
 
 def download(pl, mp3, dir):
+    forRetry = []
     if dir == '':
         dir = os.getcwd()
     print('Downloading... Please wait. If your playlist is very long this could take some time.')
-    cnt = len(pl)
-    i = 1
-    for link in pl:
-        yt = YouTube(link)
-        vid = yt.streams.filter(only_audio=mp3).first()
-        output = vid.download(output_path=dir)
-        if mp3:
-            name, _ = os.path.splitext(output)
-            try:
+    for i, link in enumerate(pl):
+        try:
+            yt = YouTube(link)
+            vid = yt.streams.filter(only_audio=mp3).first()
+            output = vid.download(output_path=dir)
+            if mp3:
+                name, _ = os.path.splitext(output)
+                name = re.sub(r'/W+', '', name)
                 os.rename(output, name + '.mp3')
-            except: 
-                #this supposedly wont be used, try is only used to bypass some in cmd usage
-                #bug interaction between os.rename with YouTube.streams.download
-                os.rename(output, '01'+name+'.mp3')
-        print('Downloaded: {0}/{1}'.format(str(i), str(cnt)))
-        i += 1
+        except:
+            print(f'Couldnt download {i}')
+            forRetry.append(link)
+            continue
+        print(f'Downloaded: {i+1}/{str(len(pl))}')
     print('Your playlist has been downloaded.')
-
-
-###
+    if len(forRetry) > 0:
+        print('Do you want to retry the failed downloads? Y/N')
+        if str(input()).upper() == 'Y':
+            download(forRetry, mp3, dir)
 
 
 print('YTPLD - Full YouTube playlist downloader')
@@ -51,9 +49,9 @@ print('The default destination will be the local directory of this executable. '
 user_input = input()
 splitter = user_input.split(' ')
 mp3 = True
-dir = ""
-if '--Video' in user_input:
+dir = ''
+if '--video' in user_input:
     mp3 = False
 if '--dir' in user_input:
-    dir = user_input[user_input.find('--dir') + len('--dir-'):]
+    dir = user_input[user_input.find('--dir') + len('--dir_'):]
 download(get_urls(splitter[0]), mp3, dir)
